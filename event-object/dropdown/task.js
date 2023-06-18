1b116b8d360f6d7617702cddc4555355ed19815d
Ivan Khokhlov <mozhno@MacBook-Air-Mozhno.local>
2023-06-18 08:26:48 +0300

    task.js in dropdown change

--- event-object/dropdown/task.js
+++ event-object/dropdown/task.js
@@ -1,12 +1,18 @@
 let dropdownList = document.querySelector('.dropdown__list');
-let dropdownValue = document.querySelector('.dropdown__value');
+let dropdownValue = document.querySelectorAll('.dropdown__value');
 let dropdownItem = Array.from(document.querySelectorAll('.dropdown__item'));
 
-dropdownValue.addEventListener('click', () => dropdownList.classList.add('dropdown__list_active'));
+dropdownValue.forEach(element => element.addEventListener('click', (e) => {
+    e.preventDefault();
+    dropdownList.classList.add('dropdown__list_active')
+}));
 
 dropdownItem.forEach(element => element.addEventListener('click', (e) => {
     e.preventDefault();
-    dropdownValue.textContent = element.textContent;
+    
+    for (let i = 0; i < dropdownValue.length; i++) {
+        dropdownValue[i].textContent = element.textContent;
+    }
     dropdownList.classList.remove('dropdown__list_active');
 }));
 
