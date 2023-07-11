b138524a61c7ab97d6a4d6fe83c92807615a2aee
Ivan Khokhlov <mozhno@MacBook-Air-Mozhno.local>
2023-07-11 12:56:16 +0300

    change task.js

--- client-state/popup/task.js
+++ client-state/popup/task.js
@@ -1,21 +1,23 @@
 const modal = document.getElementById('subscribe-modal');
 const modalClose = document.querySelector('.modal__close');
 
-if (getCookie('modalClosed') !== 'closed') {
+if (getCookie('modalClosed') !== 'closed' || getCookie('modalClosed') === undefined) {
     modal.classList.add('modal_active');
-}    
-    modalClose.addEventListener('click', () => {
-    modal.classList.remove('modal_active');
-    document.cookie='modalClosed=closed';
-});
+}
 
+modalClose.addEventListener('click', () => {
+    modal.classList.remove('modal_active');
+    setCookie('modalClosed', 'closed');
+});
 
 function setCookie(name, value) {
     document.cookie = name + '=' + encodeURIComponent(value);
 }
 
 function getCookie(name) {
-    const pairs = document.cookie.split('; ');
-    let result = pairs.find(p => p.startsWith(name + '='));
-    return result.substring(name.length + 1);
+    if (document.cookie) {
+        const pairs = document.cookie.split('; ');
+        let result = pairs.find(p => p.startsWith(name + '='));
+        return result.substring(name.length + 1);
+    }
 }
